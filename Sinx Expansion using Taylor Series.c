7041dda497df99c1e00316f0d21f385c5ccf43cf
Sadiya-025 <147088596+Sadiya-025@users.noreply.github.com>
2023-12-18 21:07:39 +0530

    Update Sinx Expansion using Taylor Series.c

--- Sinx Expansion using Taylor Series.c
+++ Sinx Expansion using Taylor Series.c
@@ -1,11 +1,3 @@
-/******************************************************************************
-
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
 //Sinx Expansion Using Taylor Series
 //Sinx = x/1! - x^3/3! + x^5/5! - x^7/7! +...
 #include <stdio.h>
