88f29bc6ae9ed6d86b261b5cbe0c6143f712d6ac
Sadiya-025 <147088596+Sadiya-025@users.noreply.github.com>
2023-12-18 21:06:19 +0530

    Update Cosx Expansion using Taylor Series.c

--- Cosx Expansion using Taylor Series.c
+++ Cosx Expansion using Taylor Series.c
@@ -1,11 +1,3 @@
-/******************************************************************************
-
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
 //Cosx Expansion Using Taylor Series
 //Cosx = 1 - x^2/2! + x^4/4! - x^6/6! +...
 #include <stdio.h>
